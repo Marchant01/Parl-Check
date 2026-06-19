28427ae24021340f9c48bd8a373866d7812ee38c
Marchant01 <celvin.marchant@hotmail.com>
2026-06-19 14:16:43 +0200

    Updated the db injection functions to correctly insert all commands from the large sql files.

--- seeder.py
+++ seeder.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from pathlib import Path
+import re
 
 from sqlalchemy import (
     create_engine,
     text
 )
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from pgvector.sqlalchemy import Vector
 
 PERSON_DATA_PATH = "documents/personer/personer"
@@ -15,19 +16,29 @@ def load_sql_paths(directory: str) -> list[Path]:
     documents_path = Path(DOCUMENTS_PATH) / directory
     return list(documents_path.glob("*.sql"))
 
-def seed_tables_sql(db: sessionmaker, path_to_directory: str):
-    paths = load_sql_paths(path_to_directory)
-    
+def seed_tables_sql(sess: Session, path_to_directory: str):
     try:
-        with db.begin():
-            for path in paths:
-                print(f"Inserting: {path}")
-                sql = path.read_text(encoding="utf-8")
-                db.execute(text(sql))
+        with sess.begin():
+            for path in load_sql_paths(path_to_directory):
+                print(f"Processing: {path.name}")
+                raw_sql = path.read_text(encoding="utf-8")
+                inserts = re.findall(r'INSERT INTO .+?^(?=INSERT|\Z)', raw_sql, re.DOTALL | re.MULTILINE)
+
+                for insert in inserts:
+                    sess.execute(text(insert).execution_options(no_parameters=True))
+
     except Exception as e:
         print(f"Error seeding {path_to_directory} tables: {e}")
 
-def seed_persons(db: sessionmaker, path_to_csv: str):
+def seed_persons(sess: Session, engine, path_to_csv: str):
+    try:
+        with sess.begin():
+            sess.execute(text("DROP TABLE IF EXISTS person CASCADE"))
+            print("Dropped old ´person´ table (cascade).")
+
+    except Exception as e:
+        print(f"Couldn't drop `person` table: {e}")
+
     columns = {
         "Förnamn": "fornamn",
         "Efternamn": "efternamn",
@@ -45,7 +56,7 @@ def seed_persons(db: sessionmaker, path_to_csv: str):
         print("Seeding person table")
         person_df_copy.to_sql(
             name="person",
-            con=db,
+            con=engine,
             if_exists="replace",
             index=False
         )
