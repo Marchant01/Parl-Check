f6e1876a5d1788aab53fca21adf36bdb76b59c7a
Marchant01 <celvin.marchant@hotmail.com>
2026-06-19 14:22:37 +0200

    Updated the seed function so that it can be called as a script to seed an existing database. The connection strings still need to be moved to a .env file though

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 from sqlalchemy import (
     create_engine,
     Integer,
+    BigInteger,
     String,
     Text,
     ForeignKey,
@@ -17,13 +18,14 @@ from pgvector.sqlalchemy import Vector
 
 import uuid
 
-pythonDATABASE_URL = "postgresql+psycopg2://gov_check_user:gov_check_pw@localhost:5432/gov_check_db"
-PATH_TO_CSV = "documents/personer"
+DATABASE_URL = "postgresql+psycopg2://gov_check_user:gov_check_pw@localhost:5432/gov_check_db"
+PATH_TO_CSV = "documents/personer/personer"
 
 engine = create_engine(
     DATABASE_URL, 
-    pool_size=5,
-    max_overflow=10,
+    pool_size=10,
+    max_overflow=20,
+    pool_timeout=30,
     pool_pre_ping=True
 )
 
@@ -50,59 +52,50 @@ class Anforande(Base):
     underrubrik: Mapped[str] = mapped_column(String(255))
     kammaraktivitet: Mapped[str] = mapped_column(String(250))
     anforande_id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True)
-    anforande_numer: Mapped[int] = mapped_column(Integer)
+    anforande_nummer: Mapped[int] = mapped_column(Integer)
     talare: Mapped[str] = mapped_column(String(250))
     parti: Mapped[str] = mapped_column(String(50))
     anforandetext: Mapped[str] = mapped_column(Text)
-    
-    intressent_id: Mapped[int] = mapped_column(Integer, ForeignKey("person.intressent_id")) # Could be a fk to intressent_id in person
-    person: Mapped["Person"] = relationship("Person", back_populates="anforanden")
-    
+    intressent_id: Mapped[str] = mapped_column(String, default=None, nullable=True) # Could be a fk to intressent_id in person
     rel_dok_id: Mapped[str] = mapped_column(String(50))
     replik: Mapped[str] = mapped_column(String(1))
     systemdatum: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-    embedding: Mapped[list[float]] = mapped_column(Vector(768)) # This must exactly match the same dimensional embeddings as the embedding model!
+    embedding: Mapped[list[float]] = mapped_column(Vector(768), nullable=True) # This must exactly match the same dimensional embeddings as the embedding model!
 
 # This table might remain unnused since the document contents are present in the Anforande table
-class Dokument(Base):
-    __tablename__ = "dokument"
-    dok_id: Mapped[str] = mapped_column(String(50), primary_key=True)
-    innehåll: Mapped[str] = mapped_column(Text)
-    embedding: Mapped[list[float]] = mapped_column(Vector(768))
+# class Dokument(Base):
+#     __tablename__ = "dokument"
+#     dok_id: Mapped[str] = mapped_column(String(50), primary_key=True)
+#     innehåll: Mapped[str] = mapped_column(Text)
+#     embedding: Mapped[list[float]] = mapped_column(Vector(768))
 
 class Person(Base):
     __tablename__ = "person"
     fornamn: Mapped[str] = mapped_column(String(80))
     efternamn: Mapped[str] = mapped_column(String(80))
     parti: Mapped[str] = mapped_column(String(40))
-    intressent_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    intressent_id: Mapped[str] = mapped_column(String, primary_key=True)
     kon: Mapped[str] = mapped_column(String(6))
     fodd: Mapped[int] = mapped_column(Integer)
     valkrets: Mapped[str] = mapped_column(String(50))
-
-    anforanden: Mapped[list["Anforande"]] = relationship("Anforande", back_populates="person")
-    votering: Mapped[list["Votering"]] = relationship("Votering", back_populates="person")
-    
-    embedding: Mapped[list[float]] = mapped_column(Vector(768))
+    embedding: Mapped[list[float]] = mapped_column(Vector(768), nullable=True)
 
 class Votering(Base):
     __tablename__ = "votering"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     rm: Mapped[str] = mapped_column(String(8))
     beteckning: Mapped[str] = mapped_column(String(6))
     hangar_id: Mapped[int] = mapped_column(Integer)
-    votering_id: Mapped[uuid.UUID] = mapped_column(Uuid, primary_key=True)
+    votering_id: Mapped[uuid.UUID] = mapped_column(Uuid, nullable=False)
     punkt: Mapped[int] = mapped_column(Integer)
     namn: Mapped[str] = mapped_column(String(250))
-
-    intressent_id: Mapped[int] = mapped_column(Integer, ForeignKey("person.intressent_id"))
-    person: Mapped["Person"] = relationship("Person", back_populates="votering")
-
+    intressent_id: Mapped[str] = mapped_column(String, nullable=True, default=None)
     parti: Mapped[str] = mapped_column(String(4))
     valkrets: Mapped[str] = mapped_column(String(50))
     valkretsnummer: Mapped[int] = mapped_column(Integer)
     iort: Mapped[str] = mapped_column(String)
     rost: Mapped[str] = mapped_column(String(20))
-    avser: Mapped[str] = mapped_column(String(10))
+    avser: Mapped[str] = mapped_column(String(20))
     votering: Mapped[str] = mapped_column(String(20))
     banknummer: Mapped[int] = mapped_column(Integer)
     fornamn: Mapped[str] = mapped_column(String(80))
@@ -110,19 +103,25 @@ class Votering(Base):
     kon: Mapped[str] = mapped_column(String(6))
     fodd: Mapped[int] = mapped_column(Integer)
     datum: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-    embedding: Mapped[list[float]] = mapped_column(Vector(768))
+    embedding: Mapped[list[float]] = mapped_column(Vector(768), nullable=True)
 
 
 def seed_all():
+    """
+    Function that uses a session to insert anforande, votering and person from sql/csv files.
+    When calling main, this will seed the entire database with the tables present in database.py.
+    """
     print("Trying to create database...")
     try:
+        Base.metadata.drop_all(engine, checkfirst=True)
         Base.metadata.create_all(engine)
         print("Tables created...")
 
-        seed_tables_sql(SessionLocal, "anforanden")
-        seed_tables_sql(SessionLocal, "voteringar")
+        with SessionLocal() as session:
+            seed_tables_sql(session, "anforanden")
+            seed_tables_sql(session, "voteringar")
 
-        seed_persons(SessionLocal, PATH_TO_CSV)
+        seed_persons(session, engine, PATH_TO_CSV)
 
         print("Database created and seeded! ")
 
