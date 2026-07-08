cd35ffb63378eb6ac52ebff4f61a451d73c3b9e8
Marchant01 <celvin.marchant@hotmail.com>
2026-07-08 23:05:38 +0200

    Started working on a navbar.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { Navbar } from './Navbar';
 import './App.css'
 
 function App() {
   return (
-    
-  )
+    <div className='app-main-container'>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App
